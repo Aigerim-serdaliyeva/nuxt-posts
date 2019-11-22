baf1b867ff968948ce5266d8298f522b314406c1
Айгерим Сердалиева <aigerim.serdaliyeva@mail.ru>
2019-11-22 17:43:16 +0600

    mongodb

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const keys = require('./keys')
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+module.exports = app
+
+
--- store/auth.js
+++ store/auth.js
@@ -1,6 +1,6 @@
 
 export const state = () => ({
-  token: true
+  token: false
 })
 
 export const getters = {
